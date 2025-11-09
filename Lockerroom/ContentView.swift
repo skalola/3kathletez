697c28ff854742fb7debb8fc63664f17f088c156
3K Sportz <3ksportz@gmail.com>
2025-11-09 14:16:03 -0500

    Initial Commit

---
+++ Lockerroom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lockerroom
+//
+//  Created by Shiv Kalola on 11/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lockerroom/LockerroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockerroomApp.swift
+//  Lockerroom
+//
+//  Created by Shiv Kalola on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LockerroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
